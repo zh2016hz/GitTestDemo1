0e6adc1656553c496979ec63ab65b191a95ada61
zhouhao9@xiaoniu66.com <zhouhao9@xiaoniu66.com>
2018-01-30 14:45:56 +0800

    增加第一个按钮的点击事件

--- app/src/main/java/com/example/xn069392/gittestprogroum/MainActivity.java
+++ app/src/main/java/com/example/xn069392/gittestprogroum/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.xn069392.gittestprogroum;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button  viewById = (Button) findViewById(R.id.bt_1);
+        viewById.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"!!!!!!!!!!!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
