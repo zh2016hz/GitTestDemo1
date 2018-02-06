719fcfa14996add53e6106693861074876b42494
zhouhao9@xiaoniu66.com <zhouhao9@xiaoniu66.com>
2018-02-06 15:07:45 +0800

    增加toast 弹窗

--- app/src/main/java/com/example/xn069392/gittestprogroum/MainActivity.java
+++ app/src/main/java/com/example/xn069392/gittestprogroum/MainActivity.java
@@ -17,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"!!!!!!!!!!!",Toast.LENGTH_LONG).show();
-            }
+                Toast.makeText(MainActivity.this,"XXXXX",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this,"",Toast.LENGTH_LONG).show();
+           }
         });
     }
 }
